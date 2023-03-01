924bde8b9da30db107aa4a86a4d203323a62236f
Danny Foster <danny@northwestern.edu>
2023-03-01 13:05:41 -0600

    fix: run the dark theme content script for MV2

--- src/entries/content-scripts/theme.ts
+++ src/entries/content-scripts/theme.ts
@@ -2,11 +2,19 @@ import { runtime } from "webextension-polyfill";
 import descriptionColor from "./descriptionColor";
 
 export default function enableDarkTheme() {
-  const darkStyles = document.createElement("link");
-  darkStyles.rel = "stylesheet";
-  darkStyles.type = "text/css";
-  darkStyles.href = runtime.getURL("background.css");
-  document.documentElement.appendChild(darkStyles);
+  /*
+    Firefox has a flash of unstyled content issue with TDX before the background
+    script is able to inject the CSS. This workaround manually injects it in the
+    content script to avoid the bug.
+  */
+  const manifestVersion = runtime.getManifest().manifest_version;
+  if (manifestVersion === 2) {
+    const darkStyles = document.createElement("link");
+    darkStyles.rel = "stylesheet";
+    darkStyles.type = "text/css";
+    darkStyles.href = runtime.getURL("background.css");
+    document.documentElement.appendChild(darkStyles);
+  }
 
   descriptionColor();
 }
